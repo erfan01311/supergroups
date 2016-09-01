7ccfbc0afc2fe3e7efa26e78553f534a74e50f81
erfan01311 <khanerfan74@gmail.com>
2016-09-01 11:58:33 +0430

    Update anti-bot.lua

--- plugins/anti-bot.lua
+++ plugins/anti-bot.lua
@@ -1,113 +1,152 @@
 
-local function isBotAllowed (userId, chatId)
-  local hash = 'anti-bot:allowed:'..chatId..':'..userId
-  local banned = redis:get(hash)
-  return banned
+do
+local function get_msgs_user_chat(user_id, chat_id)
+  local user_info = {}
+  local uhash = 'user:'..user_id
+  local user = redis:hgetall(uhash)
+  local um_hash = 'msgs:'..user_id..':'..chat_id
+  user_info.msgs = tonumber(redis:get(um_hash) or 0)
+  user_info.name = user_print_name(user)..' ['..user_id..']'
+  return user_info
 end
-
-local function allowBot (userId, chatId)
-  local hash = 'anti-bot:allowed:'..chatId..':'..userId
-  redis:set(hash, true)
-end
-
-local function disallowBot (userId, chatId)
-  local hash = 'anti-bot:allowed:'..chatId..':'..userId
-  redis:del(hash)
-end
-
--- Is anti-bot enabled on chat
-local function isAntiBotEnabled (chatId)
-  local hash = 'anti-bot:enabled:'..chatId
-  local enabled = redis:get(hash)
-  return enabled
-end
-
-local function enableAntiBot (chatId)
-  local hash = 'anti-bot:enabled:'..chatId
-  redis:set(hash, true)
-end
-
-local function disableAntiBot (chatId)
-  local hash = 'anti-bot:enabled:'..chatId
-  redis:del(hash)
-end
-
-local function isABot (user)
-  -- Flag its a bot 0001000000000000
-  local binFlagIsBot = 4096
-  local result = bit32.band(user.flags, binFlagIsBot)
-  return result == binFlagIsBot
-end
-
-local function kickUser(userId, chatId)
-  local chat = 'chat#id'..chatId
-  local user = 'user#id'..userId
-  chat_del_user(chat, user, function (data, success, result)
-    if success ~= 1 then
-      print('I can\'t kick '..data.user..' but should be kicked')
-    end
-  end, {chat=chat, user=user})
-end
-
-local function run (msg, matches)
-  -- We wont return text if is a service msg
-  if matches[1] ~= 'chat_add_user' and matches[1] ~= 'chat_add_user_link' then
-    if msg.to.type ~= 'chat' then
-      return 'Anti-flood works only on channels'
-    end
+local function chat_stats(chat_id)
+  local hash = 'chat:'..chat_id..':users'
+  local users = redis:smembers(hash)
+  local users_info = {}
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
   end
-
-  local chatId = msg.to.id
-  if matches[1] == 'enable' then
-    enableAntiBot(chatId)
-    return 'Anti-bot enabled on this chat'
-  end
-  if matches[1] == 'disable' then
-    disableAntiBot(chatId)
-    return 'Anti-bot disabled on this chat'
-  end
-  if matches[1] == 'allow' then
-    local userId = matches[2]
-    allowBot(userId, chatId)
-    return 'Bot '..userId..' allowed'
-  end
-  if matches[1] == 'disallow' then
-    local userId = matches[2]
-    disallowBot(userId, chatId)
-    return 'Bot '..userId..' disallowed'
-  end
-  if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' then
-    local user = msg.action.user or msg.from
-    if isABot(user) then
-      print('It\'s a bot!')
-      if isAntiBotEnabled(chatId) then
-        print('Anti bot is enabled')
-        local userId = user.id
-        if not isBotAllowed(userId, chatId) then
-          kickUser(userId, chatId)
-        else
-          print('This bot is allowed')
-        end
+  table.sort(users_info, function(a, b) 
+      if a.msgs and b.msgs then
+        return a.msgs > b.msgs
       end
+    end)
+  local text = 'Chat stats:\n'
+  for k,user in pairs(users_info) do
+    text = text..user.name..' = '..user.msgs..'\n'
+  end
+  return text
+end
+
+local function get_group_type(target)
+  local data = load_data(_config.moderation.data)
+  local group_type = data[tostring(target)]['group_type']
+    if not group_type or group_type == nil then
+       return 'No group type available.\nUse /type in the group to set type.'
     end
+    return group_type
+end
+
+local function get_description(target)
+  local data = load_data(_config.moderation.data)
+  local data_cat = 'description'
+  if not data[tostring(target)][data_cat] then
+    return 'No description available.'
+  end
+  local about = data[tostring(target)][data_cat]
+  return about
+end
+
+local function get_rules(target)
+  local data = load_data(_config.moderation.data)
+  local data_cat = 'rules'
+  if not data[tostring(target)][data_cat] then
+    return 'No rules available.'
+  end
+  local rules = data[tostring(target)][data_cat]
+  return rules
+end
+
+
+local function modlist(target)
+  local data = load_data(_config.moderation.data)
+  local groups = 'groups'
+  if not data[tostring(groups)] or not data[tostring(groups)][tostring(target)] then
+    return 'Group is not added or is Realm.'
+  end
+  if next(data[tostring(target)]['moderators']) == nil then
+    return 'No moderator in this group.'
+  end
+  local i = 1
+  local message = '\nList of moderators :\n'
+  for k,v in pairs(data[tostring(target)]['moderators']) do
+    message = message ..i..' - @'..v..' [' ..k.. '] \n'
+    i = i + 1
+  end
+  return message
+end
+
+local function get_link(target)
+  local data = load_data(_config.moderation.data)
+  local group_link = data[tostring(target)]['settings']['set_link']
+  if not group_link or group_link == nil then 
+    return "No link"
+  end
+  return "Group link:\n"..group_link
+end
+
+local function all(msg,target,receiver)
+  local data = load_data(_config.moderation.data)
+  if not data[tostring(target)] then
+    return
+  end
+  local text = "All the things I know about this group\n\n"
+  local group_type = get_group_type(target)
+  text = text.."Group Type: \n"..group_type
+  if group_type == "Group" or group_type == "Realm" then
+	local settings = show_group_settingsmod(msg,target)
+	text = text.."\n\n"..settings
+  elseif group_type == "SuperGroup" then
+	local settings = show_supergroup_settingsmod(msg,target)
+	text = text..'\n\n'..settings
+  end
+  local rules = get_rules(target)
+  text = text.."\n\nRules: \n"..rules
+  local description = get_description(target)
+  text = text.."\n\nAbout: \n"..description
+  local modlist = modlist(target)
+  text = text.."\n\nMods: \n"..modlist
+  local link = get_link(target)
+  text = text.."\n\nLink: \n"..link
+  local stats = chat_stats(target)
+  text = text.."\n\n"..stats
+  local mutes_list = mutes_list(target)
+  text = text.."\n\n"..mutes_list
+  local muted_user_list = muted_user_list(target)
+  text = text.."\n\n"..muted_user_list
+  local ban_list = ban_list(target)
+  text = text.."\n\n"..ban_list
+  local file = io.open("./groups/all/"..target.."all.txt", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  send_document(receiver,"./groups/all/"..target.."all.txt", ok_cb, false)
+  return
+end
+
+local function run(msg, matches)
+  if matches[1] == "all" and matches[2] and is_owner2(msg.from.id, matches[2]) then
+    local receiver = get_receiver(msg)
+    local target = matches[2]
+    return all(msg,target,receiver)
+  end
+  if not is_owner(msg) then
+    return
+  end
+  if matches[1] == "all" and not matches[2] then
+    local receiver = get_receiver(msg)
+    return all(msg,msg.to.id,receiver)
   end
 end
+
 
 return {
-  description = 'When bot enters group kick it.',
-  usage = {
-    '!antibot enable: Enable Anti-bot on current chat',
-    '!antibot disable: Disable Anti-bot on current chat',
-    '!antibot allow <botId>: Allow <botId> on this chat',
-    '!antibot disallow <botId>: Disallow <botId> on this chat'
-  },
   patterns = {
-    '^!antibot (allow) (%d+)$',
-    '^!antibot (disallow) (%d+)$',
-    '^!antibot (enable)$',
-    '^!antibot (disable)$',
-    '^!!tgservice (chat_add_user)$',
-    '^!!tgservice (chat_add_user_link)$'
+  "^[#!/](all)$",
+  "^[#!/](all) (%d+)$"
   },
   run = run
 }
+end

99f0431a2b92d9ae71e2d2e82c5ac88a78b93bd6
erfan01311 <khanerfan74@gmail.com>
2016-09-01 12:58:14 +0430

    Update face.lua

--- plugins/face.lua
+++ plugins/face.lua
@@ -1,92 +1,150 @@
-local https = require("ssl.https")
-local ltn12 = require "ltn12"
+do
 
--- Edit data/mashape.lua with your Mashape API key
--- http://docs.mashape.com/api-keys
-local mashape = load_from_file('data/mashape.lua', {
-      api_key = ''
-   })
-
-local function request(imageUrl)
-   local api_key = mashape.api_key
-   if api_key:isempty() then
-      return nil, 'Configure your Mashape API Key'
-   end
-
-   local api = "https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?"
-   local parameters = "attribute=gender%2Cage%2Crace"
-   parameters = parameters .. "&url="..(URL.escape(imageUrl) or "")
-   local url = api..parameters
-   local headers = {
-      ["X-Mashape-Key"] = api_key,
-      ["Accept"] = "Accept: application/json"
-   }
-   print(url)
-   local respbody = {}
-   local body, code = https.request{
-      url = url,
-      method = "GET",
-      headers = headers,
-      sink = ltn12.sink.table(respbody),
-      protocol = "tlsv1"
-   }
-   if code ~= 200 then return "", code end
-   local body = table.concat(respbody)
-   return body, code
+-- Returns a table with `name` and `msgs`
+local function get_msgs_user_chat(user_id, chat_id)
+  local user_info = {}
+  local uhash = 'user:'..user_id
+  local user = redis:hgetall(uhash)
+  local um_hash = 'msgs:'..user_id..':'..chat_id
+  user_info.msgs = tonumber(redis:get(um_hash) or 0)
+  user_info.name = user_print_name(user)..' ['..user_id..']'
+  return user_info
 end
 
-local function parseData(data)
-   local jsonBody = json:decode(data)
-   local response = ""
-   if jsonBody.error ~= nil then
-      if jsonBody.error == "IMAGE_ERROR_FILE_TOO_LARGE" then
-         response = response .. "The image is too big. Provide a smaller image."
-      elseif jsonBody.error == "IMAGE_ERROR_FAILED_TO_DOWNLOAD" then
-         response = response .. "Is that a valid url for an image?"
-      else
-         response = response .. jsonBody.error
-      end
-   elseif jsonBody.face == nil or #jsonBody.face == 0 then
-      response = response .. "No faces found"
-   else
-      response = response .. #jsonBody.face .." face(s) found:\n\n"
-      for k,face in pairs(jsonBody.face) do
-         local raceP = ""
-         if face.attribute.race.confidence > 85.0 then
-            raceP = face.attribute.race.value:lower()
-         elseif face.attribute.race.confidence > 50.0 then
-            raceP = "(probably "..face.attribute.race.value:lower()..")"
-         else
-            raceP = "(posibly "..face.attribute.race.value:lower()..")"
-         end
-         if face.attribute.gender.confidence > 85.0 then
-            response = response .. "There is a "
-         else
-            response = response .. "There may be a "
-         end
-         response = response .. raceP .. " " .. face.attribute.gender.value:lower() .. " "
-         response = response .. ", " .. face.attribute.age.value .. "(±".. face.attribute.age.range ..") years old \n"
-      end
-   end
-   return response
+local function chat_stats(receiver, chat_id)
+  -- Users on chat
+  local hash = 'chat:'..chat_id..':users'
+  local users = redis:smembers(hash)
+  local users_info = {}
+  -- Get user info
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
+  end
+  -- Sort users by msgs number
+  table.sort(users_info, function(a, b) 
+    if a.msgs and b.msgs then
+        return a.msgs > b.msgs
+    end
+  end)
+  local text = 'Users in this chat \n'
+  for k,user in pairs(users_info) do
+    text = text..user.name..' = '..user.msgs..'\n'
+  end
+  local file = io.open("./groups/lists/"..chat_id.."stats.txt", "w")
+  file:write(text)
+  file:flush()
+  file:close() 
+  send_document(receiver,"./groups/lists/"..chat_id.."stats.txt", ok_cb, false)
+  return --text
 end
 
+local function chat_stats2(chat_id)
+  -- Users on chat
+  local hash = 'chat:'..chat_id..':users'
+  local users = redis:smembers(hash)
+  local users_info = {}
+
+  -- Get user info
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
+  end
+
+  -- Sort users by msgs number
+  table.sort(users_info, function(a, b) 
+      if a.msgs and b.msgs then
+        return a.msgs > b.msgs
+      end
+    end)
+
+  local text = 'Users in this chat \n'
+  for k,user in pairs(users_info) do
+    text = text..user.name..' = '..user.msgs..'\n'
+  end
+  return text
+end
+-- Save stats, ban user
+local function bot_stats()
+
+  local redis_scan = [[
+    local cursor = '0'
+    local count = 0
+    repeat
+    local r = redis.call("SCAN", cursor, "MATCH", KEYS[1])
+    cursor = r[1]
+    count = count + #r[2]
+    until cursor == '0'
+    return count]]
+
+  -- Users
+  local hash = 'msgs:*:'..our_id
+  local r = redis:eval(redis_scan, 1, hash)
+  local text = 'Users: '..r
+
+  hash = 'chat:*:users'
+  r = redis:eval(redis_scan, 1, hash)
+  text = text..'\nGroups: '..r
+  return text
+end
 local function run(msg, matches)
-   --return request('http://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/m(01-32)_gr.jpg')
-   local data, code = request(matches[1])
-   if code ~= 200 then return "There was an error. "..code end
-   return parseData(data)
+  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+    local about = _config.about_text
+    local name = user_print_name(msg.from)
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    return about
+  end 
+  if matches[1]:lower() == "statslist" then
+    if not is_momod(msg) then
+      return "For mods only !"
+    end
+    local chat_id = msg.to.id
+    local name = user_print_name(msg.from)
+    savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
+    return chat_stats2(chat_id)
+  end
+  if matches[1]:lower() == "stats" then
+    if not matches[2] then
+      if not is_momod(msg) then
+        return "For mods only !"
+      end
+      if msg.to.type == 'chat' or msg.to.type == 'channel' then
+	    local receiver = get_receiver(msg)
+        local chat_id = msg.to.id
+        local name = user_print_name(msg.from)
+        savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
+        return chat_stats(receiver, chat_id)
+      else
+        return
+      end
+    end
+    if matches[2] == "teleseed" then -- Put everything you like :)
+      if not is_admin1(msg) then
+        return "For admins only !"
+      else
+        return bot_stats()
+      end
+    end
+    if matches[2] == "group" then
+      if not is_admin1(msg) then
+        return "For admins only !"
+      else
+        return chat_stats(matches[3])
+      end
+    end
+  end
 end
 
 return {
-   description = "Who is in that photo?",
-   usage = {
-      "!face [url]",
-      "!recognise [url]"
-   },
-   patterns = {
-      "^!face (.*)$",
-      "^!recognise (.*)$"
-   },
-   run = run
+  patterns = {
+    "^[#!/]([Ss]tats)$",
+    "^[#!/]([Ss]tatslist)$",
+    "^[#!/]([Ss]tats) (group) (%d+)",
+    "^[#!/]([Ss]tats) (teleseed)",
+	"^[#!/]([Tt]eleseed)"
+    }, 
+  run = run
 }
+end
